399cc225b286b043a25d40fe07aaea3beef458ac
unknown <eng.mahmoud.adel.labib@gmail.com>
2021-02-15 02:13:24 +0200

    adding registeration form

--- js/pricing_plans.js
+++ js/pricing_plans.js
@@ -8,5 +8,44 @@ $(function() {
         // $('.card .card-body .check-true').addClass('active');
         // $('.card .card-body .check-true i').addClass('active');
     });
+
+
+    // changing check-true at payment gateway section
+    $('.payment .paypal button').on('click', function () {
+        if($('.payment .paypal button .check-true i').hasClass('active')) {
+            $('.payment .paypal button .check-true i').css('display', 'inline');
+            $('.payment .bank button .check-true i').css('display', 'none');
+            $('.payment .bank button .check-true i').removeClass('active');
+        } else {
+            $('.payment .paypal button .check-true i').addClass('active');
+            $('.payment .paypal button .check-true i').css('display', 'inline');
+            $('.payment .bank button .check-true i').css('display', 'none');
+            $('.payment .bank button .check-true i').removeClass('active');
+        }
+    });
+
+    $('.payment .bank button').on('click', function () {
+        if($('.payment .bank button .check-true i').hasClass('active')) {
+            $('.payment .bank button .check-true i').css('display', 'inline');
+            $('.payment .paypal button .check-true i').css('display', 'none');
+            $('.payment .paypal button .check-true i').removeClass('active');
+        } else {
+            $('.payment .bank button .check-true i').addClass('active');
+            $('.payment .bank button .check-true i').css('display', 'inline');
+            $('.payment .paypal button .check-true i').css('display', 'none');
+            $('.payment .paypal button .check-true i').removeClass('active');
+        }
+    });
+
+    // toggle the pricing plan section
+    $('.pricing-plan .next-step button').on('click', function () {
+        $('.pricing-plan').fadeOut(700);
+        $('.registeration-form').fadeIn(1000);
+    });
+
+    $('.registeration-form .back button').on('click', function () {
+        $('.registeration-form').fadeOut(700);
+        $('.pricing-plan').fadeIn(1000);
+    });
     
 })
