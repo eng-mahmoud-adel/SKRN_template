2d686e651e644a1913aee1da8d783bdc7076a8fc
unknown <eng.mahmoud.adel.labib@gmail.com>
2021-02-17 00:26:14 +0200

    changing DBID to 1 for testing

--- js/FAQS.js
+++ js/FAQS.js
@@ -10,10 +10,10 @@ document.getElementById("form").addEventListener("submit", async function(event)
     loggedIn_user.password = password;
     
     // sending username and password from loggedIn_user object
-    let data_sent = await fetch(`http://anyservice.imassoft.com/1907${end_point}`, {
+    let data_sent = await fetch(`http://anyservice.imassoft.com/1${end_point}`, {
         method: "POST",
         headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "*",
         "Access-Control-Allow-Headers": "Content-Type"
