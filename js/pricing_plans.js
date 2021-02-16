32c9cbc9295e3c4ce30f5dab9a030979c5cadea8
unknown <eng.mahmoud.adel.labib@gmail.com>
2021-02-16 19:05:16 +0200

    fixing issue at check-true div at pricing plan section

--- js/pricing_plans.js
+++ js/pricing_plans.js
@@ -1,13 +1,39 @@
 $(function() {
-    // changing background for cards
+    // changing background for cards at pricing plan section
     $('.card').on('click', function() {
         $(this).addClass('active');
         $(this).siblings().removeClass('active');
 
-        // toggle the check-true div
-        // $('.card .card-body .check-true').addClass('active');
-        // $('.card .card-body .check-true i').addClass('active');
+        // toggle the check-true div at pricing plan section
+        $('.card.active .card-body .card-title').each(function (index, element) {
+            if($(element).text() == 'Free title') {
+                if($('.card .card-body .check-true').hasClass('active') && $('.card .card-body .card-title').text() != 'Free title') {
+                    $('.card .card-body .check-true').removeClass('active');
+                    $('.card .card-body .check-true i').removeClass('active');
+                }
+
+                $('.card.active .card-body .check-true').addClass('active');
+                $('.card.active .card-body .check-true i').addClass('active');
+            } else if($(element).text() == 'Starter') {
+                if($('.card .card-body .check-true').hasClass('active') && $('.card .card-body .card-title').text() != 'Starter') {
+                    $('.card .card-body .check-true').removeClass('active');
+                    $('.card .card-body .check-true i').removeClass('active');
+                }
+
+                $('.card.active .card-body .check-true').addClass('active');
+                $('.card.active .card-body .check-true i').addClass('active');
+            } else if($(element).text() == 'Premium') {
+                if($('.card .card-body .check-true').hasClass('active') && $('.card .card-body .card-title').text() != 'Premium') {
+                    $('.card .card-body .check-true').removeClass('active');
+                    $('.card .card-body .check-true i').removeClass('active');
+                }
+
+                $('.card.active .card-body .check-true').addClass('active');
+                $('.card.active .card-body .check-true i').addClass('active');
+            }
+        });
     });
+
 
 
     // changing check-true at payment gateway section
@@ -37,7 +63,7 @@ $(function() {
         }
     });
 
-    // toggle the pricing plan section
+    // toggle the pricing plan section and registeration form
     $('.pricing-plan .next-step button').on('click', function () {
         $('.pricing-plan').fadeOut(700);
         $('.registeration-form').fadeIn(1000);
@@ -48,4 +74,4 @@ $(function() {
         $('.pricing-plan').fadeIn(1000);
     });
     
-})
+});
