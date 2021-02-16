d41d7a206b53b1178cdadede1f9e8e2a7f0292d0
ahmedheaba <ahmedheaba1911@gmail.com>
2021-02-16 05:50:59 +0200

    Add Footer And Button To Scroll Top

--- js/home.js
+++ js/home.js
@@ -4,5 +4,18 @@ $(function(){
             $("body").css({"overflow":"auto"})
             $(this).remove()
         })
+    var scrollButton = $(".scroll-up")
+    $(window).scroll(function(){
+
+        if($(this).scrollTop() >= 500){
+            scrollButton.show(1000)
+        }else{
+            scrollButton.hide(1000)
+        }
+    })
+    scrollButton.click(function(){
+        $("html").animate({scrollTop:0},600)
+    })
+
 
 })})
