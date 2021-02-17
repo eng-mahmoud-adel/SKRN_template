80b13a25eb866a7ed9c4b25449a0b5cb78b71c5e
unknown <eng.mahmoud.adel.labib@gmail.com>
2021-02-17 20:01:17 +0200

    adding php pages for deployment purpose

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
     include_once('index.html');
+    include_once('dashboard.html');
+    include_once('display.html');
+    include_once('edit_profile.html');
+    include_once('FAQS.html');
+    include_once('pricing_plans.html');
 ?>
