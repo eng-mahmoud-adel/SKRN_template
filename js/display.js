d75708230c0d409be239c476a00aa6694644baea
ahmedheaba <ahmedheaba1911@gmail.com>
2021-02-19 20:37:44 +0200

    Add The Rating Of Any User

--- js/display.js
+++ js/display.js
@@ -35,3 +35,27 @@ const req = async () => {
     $('.movie_title').text(json.data.title);
   };
   req();
+
+$(function(){
+  $("#s1").on("click", function(){
+    $(".fa-star").css({"color":"black"})
+    $("#s1").css({"color":"yellow"})
+  })
+  $("#s2").on("click", function(){
+    $(".fa-star").css({"color":"black"})
+    $("#s1,#s2").css({"color":"yellow"})
+  })
+  $("#s3").on("click", function(){
+    $(".fa-star").css({"color":"black"})
+    $("#s1, #s2, #s3").css({"color":"yellow"})
+  })
+  $("#s4").on("click", function(){
+    $(".fa-star").css({"color":"black"})
+    $("#s1, #s2, #s3, #s4").css({"color":"yellow"})
+  })
+  $("#s5").on("click", function(){
+    $(".fa-star").css({"color":"black"})
+    $(".fa-star").css({"color":"yellow"})
+  })
+
+})
